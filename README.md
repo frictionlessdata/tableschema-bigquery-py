9471fb107b103aaae15347b41e56209b6f7ae0f0
roll <roll@respect31.com>
2015-12-02 19:17:54 +0300

    added bigquery example and auth instruction

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # jsontableschema-bigquery-py
+
 Generate and load BigQuery tables based on JSON Table Schema descriptors.
+
+## Usage
+
+### Authentification
+
+To start using Google BigQuery service:
+- Create a project - [link](https://console.developers.google.com/home/dashboard)
+- Create a service key - [link](https://console.developers.google.com/apis/credentials)
+- Save service key as `~/.google-cloud/.google-cloud-credentials.json` (as example)
+- Add environment variable `GOOGLE_APPLICATION_CREDENTIALS=~/.google-cloud/.google-cloud-credentials.json`
---
+++ examples/bigquery.py
@@ -0,0 +1,50 @@
+import argparse
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.client import GoogleCredentials
+
+
+def main(project_id):
+    # [START build_service]
+    # Grab the application's default credentials from the environment.
+    credentials = GoogleCredentials.get_application_default()
+    # Construct the service object for interacting with the BigQuery API.
+    bigquery_service = build('bigquery', 'v2', credentials=credentials)
+    # [END build_service]
+
+    try:
+        # [START run_query]
+        query_request = bigquery_service.jobs()
+        query_data = {
+            'query': (
+                'SELECT TOP(corpus, 10) as title, '
+                'COUNT(*) as unique_words '
+                'FROM [publicdata:samples.shakespeare];')
+        }
+
+        query_response = query_request.query(
+            projectId=project_id,
+            body=query_data).execute()
+        # [END run_query]
+
+        # [START print_results]
+        print('Query Results:')
+        for row in query_response['rows']:
+            print('\t'.join(field['v'] for field in row['f']))
+        # [END print_results]
+
+    except HttpError as err:
+        print('Error: {}'.format(err.content))
+        raise err
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('project_id', help='Your Google Cloud Project ID.')
+
+    args = parser.parse_args()
+
+    main(args.project_id)
