aa5a055d783d4dabff9332a888d2e367cc61c6ba
roll <roll@post.agency>
2015-12-29 12:31:45 +0300

    fixed system tests

--- tests/system/test_resource.py
+++ tests/system/test_resource.py
@@ -44,7 +44,7 @@ class TestResource(unittest.TestCase):
 
         # Run example
         scope = run(
-            table_id='resource_test_%s' % self.version,
+            table='resource_test_%s' % self.version,
             export_schema_path=self.export_schema_path,
             export_data_path=self.export_data_path)
 
